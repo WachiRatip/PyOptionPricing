7e8782b111599dd3ab81cfa0e22b351252061267
Wachiraphong Ratiphaphongthon <wachirapongr@outlook.com>
2023-03-11 20:16:17 +0700

    update example of used cases

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from option_pricing.bimodel import BinomialModel
+from option_pricing.trimodel import TrinomialModel
 from option_pricing.options import EuropeanCallOption, EuropeanPutOption, AsianCallOption, AsianPutOption
 
-if __name__=="__main__":
+
+def display_binomial_model():
+    print("Start Binomial Pricing Model")
     S0 = 100
     u = 0.1
     d = -0.1
@@ -14,19 +17,56 @@ if __name__=="__main__":
                 up = u,
                 down = d,
                 rate = r,
-                step = N
+                n_step = N
             )
     
-    print("European Options:")
+    print("-European Options:")
     option = EuropeanCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
     option = EuropeanPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
 
-    print("Asian Options:")
+    print("-Asian Options:")
     option = AsianCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
     option = AsianPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
+    print()
+
+def display_trinomial_model():
+    print("Start Trinomial Pricing Model")
+    S0 = 100.0
+    sigma = 0.1
+    T = 1.0
+    r = 0.0
+    N = 3
+    K = 130.0
+
+    model = TrinomialModel(
+                spot = S0,
+                sigma= sigma,
+                rate = r,
+                m_time= T,
+                n_step = N
+    )
+
+    print("-European Options:")
+    option = EuropeanCallOption(model = model, strike = K)
+    print("--Call Option price: ", option.get_option_price())
+
+    option = EuropeanPutOption(model = model, strike = K)
+    print("--Put Option price : ", option.get_option_price())
+
+    print("-Asian Options:")
+    option = AsianCallOption(model = model, strike = K)
+    print("--Call Option price: ", option.get_option_price())
+
+    option = AsianPutOption(model = model, strike = K)
+    print("--Put Option price : ", option.get_option_price())
+    print()
+
+if __name__=="__main__":
+    display_binomial_model()
+    display_trinomial_model()
