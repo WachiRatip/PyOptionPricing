778841cf2f85b789b8ebdf5a9b4014728ef5ad52
Wachiraphong Ratiphaphongthon <wachirapongr@outlook.com>
2023-03-13 11:42:21 +0700

    add monte carlo example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from option_pricing.bimodel import BinomialModel
 from option_pricing.trimodel import TrinomialModel
-from option_pricing.options import EuropeanCallOption, EuropeanPutOption, AsianCallOption, AsianPutOption
+from option_pricing.bsmodel import BlackScholesModel
+from option_pricing import monte_carlo, options
 
 
 def display_binomial_model():
@@ -21,17 +22,17 @@ def display_binomial_model():
             )
     
     print("-European Options:")
-    option = EuropeanCallOption(model = model, strike = K)
+    option = options.EuropeanCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
-    option = EuropeanPutOption(model = model, strike = K)
+    option = options.EuropeanPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
 
     print("-Asian Options:")
-    option = AsianCallOption(model = model, strike = K)
+    option = options.AsianCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
-    option = AsianPutOption(model = model, strike = K)
+    option = options.AsianPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
     print()
 
@@ -53,20 +54,47 @@ def display_trinomial_model():
     )
 
     print("-European Options:")
-    option = EuropeanCallOption(model = model, strike = K)
+    option = options.EuropeanCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
-    option = EuropeanPutOption(model = model, strike = K)
+    option = options.EuropeanPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
 
     print("-Asian Options:")
-    option = AsianCallOption(model = model, strike = K)
+    option = options.AsianCallOption(model = model, strike = K)
     print("--Call Option price: ", option.get_option_price())
 
-    option = AsianPutOption(model = model, strike = K)
+    option = options.AsianPutOption(model = model, strike = K)
     print("--Put Option price : ", option.get_option_price())
+    print()
+
+def display_monte_carlo_model():
+    print("Start Monte Carlo Pricing Model")
+    S0 = 100.0
+    sigma = 0.1
+    T = 1.0
+    r = 0.0
+    step = 3
+    K = 130.0
+    n_sim = 1000
+    model = BlackScholesModel(S0, sigma, r, T, step)
+
+    print("-European Options:")
+    option = monte_carlo.EuropeanCallOption(model = model, strike = K)
+    print("--Call Option price: %.4f with error %.4f" %option.get_option_price(n_sim))
+
+    option = monte_carlo.EuropeanPutOption(model = model, strike = K)
+    print("--Put Option price : %.4f with error %.4f" %option.get_option_price(n_sim))
+
+    print("-Asian Options:")
+    option = monte_carlo.AsianCallOption(model = model, strike = K)
+    print("--Call Option price: %.4f with error %.4f" %option.get_option_price(n_sim))
+
+    option = monte_carlo.AsianPutOption(model = model, strike = K)
+    print("--Put Option price : %.4f with error %.4f" %option.get_option_price(n_sim))
     print()
 
 if __name__=="__main__":
     display_binomial_model()
     display_trinomial_model()
+    display_monte_carlo_model()
