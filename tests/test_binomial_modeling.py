5601749b0d810895edfa0f508841f9307a8bf38d
Wachiraphong Ratiphaphongthon <wachirapongr@outlook.com>
2023-03-19 10:26:36 +0700

    add tests for binomial model with sigma and m_time

--- tests/test_binomial_modeling.py
+++ tests/test_binomial_modeling.py
@@ -72,3 +72,74 @@ def test_get_price(S0,u,d,r,N):
             assert model.get_price(x, i)==price
 
         model.clear_path()
+
+@pytest.mark.parametrize(
+    "S0,sigma,m_time,r,N",
+    [(100,0.3,1,0.0,3),
+     (100,0.3,1,0.05,3)]
+)
+def test_option_modeling_volatility_m_time(S0,sigma,m_time,r,N):
+    model = BinomialModel(
+        spot=S0,
+        sigma=sigma,
+        rate=r,
+        m_time=m_time,
+        n_step=N
+    )
+    total_prob = 0
+    previous_path = None
+    previous_prices = None
+    for x in range(model.total_path):
+        model.get_path(x)
+        model.get_path_prices()
+        _prob = model.get_path_prob()
+        total_prob += _prob
+        _path = model.path
+        _price = model.prices
+        assert len(_path)==model.n_step
+        assert len(_price)==model.n_step
+        for idx, val in enumerate(_path):
+            # val = 1 if up otherwise 0
+            if (idx==0):
+                if (val==1):
+                    assert S0<_price[idx]
+                else:
+                    assert S0>_price[idx]
+            else:
+                if (val==1):
+                    assert _price[idx-1]<_price[idx]
+                else:
+                    assert _price[idx-1]>_price[idx]
+        
+        # check path/price not the same
+        current_path = _path
+        current_prices = _price
+        if previous_path:
+            assert (current_path != previous_path)
+            assert (current_prices != previous_prices)
+        previous_path = _path
+        previous_path = _price
+        model.clear_path()
+    
+    assert isclose(total_prob, 1.0)
+
+@pytest.mark.parametrize(
+    "S0,sigma,m_time,r,N",
+    [(100,0.3,1,0.0,3),
+     (100,0.3,1,0.05,3)]
+)
+def test_get_price_volatility_m_time(S0,sigma,m_time,r,N):
+    model = BinomialModel(
+        spot=S0,
+        sigma=sigma,
+        rate=r,
+        m_time=m_time,
+        n_step=N
+    )
+    for x in range(model.total_path):
+        model.get_path(x)
+        model.get_path_prices()
+        for i, price in enumerate(model.prices):
+            assert model.get_price(x, i)==price
+
+        model.clear_path()
--- tests/test_binomial_pricing.py
+++ tests/test_binomial_pricing.py
@@ -21,6 +21,23 @@ def test_european_call_option_pricing(S0,u,d,r,N,K,expected):
     assert isclose(option.get_option_price(), expected)
 
 @pytest.mark.parametrize(
+    "S0,sigma,m_time,r,N,K,expected",
+    [(100,0.3,1,0.0,3,130,3.635430762),
+     (100,0.3,1,0.05,3,130,4.67440136)]
+)
+def test_european_call_option_pricing_volatility_m_time(S0,sigma,m_time,r,N,K,expected):
+    model = BinomialModel(
+                spot = S0,
+                sigma = sigma,
+                m_time = m_time,
+                rate = r,
+                n_step = N
+            )
+    
+    option = EuropeanCallOption(model = model, strike = K)
+    assert isclose(option.get_option_price(), expected)
+
+@pytest.mark.parametrize(
     "S0,u,d,r,N,K,expected",
     [(100,0.1,-0.1,0.0,3,130,30.3875),
      (100,0.1,-0.1,0.05,3,130,13.42862542)]
@@ -30,6 +47,23 @@ def test_european_put_option_pricing(S0,u,d,r,N,K,expected):
                 spot = S0,
                 up = u,
                 down = d,
+                rate = r,
+                n_step = N
+            )
+    
+    option = EuropeanPutOption(model = model, strike = K)
+    assert isclose(option.get_option_price(), expected)
+
+@pytest.mark.parametrize(
+    "S0,sigma,m_time,r,N,K,expected",
+    [(100,0.3,1,0.0,3,130,33.63543076),
+     (100,0.3,1,0.05,3,130,28.33422654)]
+)
+def test_european_put_option_pricing_volatility_m_time(S0,sigma,m_time,r,N,K,expected):
+    model = BinomialModel(
+                spot = S0,
+                sigma = sigma,
+                m_time = m_time,
                 rate = r,
                 n_step = N
             )
